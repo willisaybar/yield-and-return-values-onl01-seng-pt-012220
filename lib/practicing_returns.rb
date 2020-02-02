589ab82ce7ced0344df2dfa2bf7ccd2e30e64922
Willis Aybar <willisaybar@gmail.com>
2020-02-02 00:10:38 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,10 +3,13 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
-    yield(array[i])
+  binding.pry
+    collection << yield(array[i])
     i += 1
   end
+  colleciton
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
